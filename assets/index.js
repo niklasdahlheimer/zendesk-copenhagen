09dffe3fc9ec8b58ff4c1d643e94a20b3f98321b
Niklas Dahlheimer <dahlheimer@tentaclesync.com>
2024-12-16 16:42:19 +0100

    add new mean menu

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,18 @@
 import "./index.scss";
 
 import "./modal.jquery.js";
-import "./meanmenu.jquery.js";
 import "./infieldLabel.jquery.js";
 
+import "./meanMenu/jquery.meanmenu.js";
+
 $(document).ready(function (){
+
+  // social share popups
+  $(".share a").click(function (e) {
+    e.preventDefault();
+    window.open(this.href, "", "height = 500, width = 500");
+  });
+
 
   $(".fixed-menu").meanmenu({
     onePage: false,
